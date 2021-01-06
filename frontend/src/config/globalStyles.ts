68c0f6664f95941e8d33c594bceb2bb00d0a580d
mati <mateuszlibero@gmail.com>
2021-01-06 17:11:40 +0100

    home views without forms

---
+++ frontend/src/config/globalStyles.ts
@@ -0,0 +1,1 @@
+export const mainSpacing = 15;
--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -1,7 +1,26 @@
+import { Box, SimpleGrid, Center } from "@chakra-ui/react";
 import { FC } from "react";
+import { mainSpacing } from "../../config/globalStyles";
 
 const Home: FC = () => {
-    return <p>elo</p>;
+  return (
+    <SimpleGrid columns={2} spacing={mainSpacing}>
+      <Box bg="black">
+        <Center bg="black" height="100vh">
+          <p>
+            text
+          </p>
+        </Center>
+      </Box>
+      <Box bg="white">
+        <Center height="100vh">
+          <p>
+            text
+          </p>
+        </Center>
+      </Box>
+    </SimpleGrid>
+  )
 }
 
 export default Home;
