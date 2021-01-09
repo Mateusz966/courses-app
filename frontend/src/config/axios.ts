d923c14c11f572b7c35dde0c065acc3f80efc663
mati <mateuszlibero@gmail.com>
2021-01-09 18:38:40 +0100

    axios + axios wrapper in rxjs

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,18 +1,6 @@
-import { AxiosRequestConfig, Method } from "axios";
-import { apiUrl } from "./apiUrl";
+import { AxiosRequestConfig } from 'axios';
+import { apiUrl } from './apiUrl';
 
-export const axiosConfig = <T>(
-    path: string, 
-    method: Method, 
-    data?: T, 
-    params?: string, 
-    timeout?: number
- ): AxiosRequestConfig => ({
-    withCredentials: true,
-    timeout: timeout ? timeout : 6000,
-    url: apiUrl + path,
-    method,
-    data,
-    params,
-  });
-  
+export const axiosRequestConfiguration: AxiosRequestConfig = {
+    baseURL: apiUrl,
+};
