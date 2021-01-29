3775532eaa6bf49ac221391f89ea1d6bb38c4e4f
mati <mateuszlibero@gmail.com>
2021-01-29 19:05:57 +0100

    ended login and register tests

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -4,13 +4,34 @@ describe('Login tests', () => {
   });
 
   it('Login form should validate sent data', () => {
-    cy.get('input[name=email]');
-    cy.get('input[name=password]');
+    cy.get('input[name=email]').type('123123');
+    cy.get('input[name=password]').type('123123');;
+
+    cy.get('.chakra-form__error-message ').should('exist');
+
+    cy.get('button').first().should('be.disabled');
+
+    
+    cy.get('input[name=email]').clear();
+    cy.get('input[name=password]').clear();;
+  });
+
+
+  it('Should occur an error and ', () => {
+    const email = 'mateuszaaalibero@gmail.com';
+    const pwd = '123qwddde';
+
+    cy.get('input[name=email]').type(email);
+    cy.get('input[name=password]').type(pwd);
 
     cy.get('button').first().click();
 
-    // cy.get('.formfield-input-error').should('exist');
+    cy.url().should('include', '/sign-in');
+
+    cy.get('input[name=email]').clear();
+    cy.get('input[name=password]').clear();
   });
+
 
   it('Login on success should redirect to dashboard', () => {
     const email = 'mateuszlibero@gmail.com';
@@ -18,10 +39,10 @@ describe('Login tests', () => {
 
     cy.get('input[name=email]').type(email);
     cy.get('input[name=password]').type(pwd);
-    cy.get('button').click();
 
-    
-    cy.url().should('include', '/dashboard')
+    cy.get('button').first().click();
+
+    cy.url().should('include', '/')
   });
 
 });
