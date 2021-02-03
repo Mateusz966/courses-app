6559d36b1025776f724afe94fe76348cb862f920
grubapanda <staciwapiotr1@gmail.com>
2021-02-03 20:53:36 +0100

    TopNavbar added

--- frontend/src/components/common/NavUserActions/LoggedActions/index.tsx
+++ frontend/src/components/common/NavUserActions/LoggedActions/index.tsx
@@ -9,15 +9,24 @@ import {
   Icon,
 } from '@chakra-ui/react';
 import React from 'react';
+import { FC } from 'react';
 import { MdKeyboardArrowDown, MdShoppingCart } from 'react-icons/md';
 
-export const LoggedActions = () => {
+interface Props {
+  justifyContentType?: string;
+}
+
+export const LoggedActions: FC<Props> = ({
+  justifyContentType
+}) => {
   return (
-    <HStack>
+    <HStack justifyContent={justifyContentType}>
       <IconButton
         bgColor="#fff"
+        color="#4A5568"
         aria-label="Shopping Cart"
         icon={<Icon w="6" h="6" as={MdShoppingCart} />}
+        display={{ md: 'block', base: 'none' }}
       />
       <Menu>
         <MenuButton
@@ -26,10 +35,11 @@ export const LoggedActions = () => {
           as={Button}
           rightIcon={<MdKeyboardArrowDown />}
         >
-          Moje konto
+          Moje konto 
         </MenuButton>
-        <MenuList>
+        <MenuList  fontSize="16px">
           <MenuItem>Moje kursy</MenuItem>
+          <MenuItem>Ustawienia</MenuItem>
         </MenuList>
       </Menu>
     </HStack>
--- frontend/src/components/common/NavUserActions/index.tsx
+++ frontend/src/components/common/NavUserActions/index.tsx
@@ -1,12 +1,33 @@
-import { Box } from '@chakra-ui/react';
+import { Box, IconButton, Icon, HStack } from '@chakra-ui/react';
 import React from 'react';
+import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../config/store';
 import { LoggedActions } from './LoggedActions';
 import { NotLoggedActions } from './NotLoggedActions';
+import { MdShoppingCart } from 'react-icons/md';
 
-export const NavUserActions = () => {
+interface Props {
+  justifyContentType?: string;
+}
+
+export const NavUserActions: FC<Props> = ({
+  justifyContentType
+}) => {
   const user = useSelector((state: RootState) => state.user?.details);
 
-  return <Box>{user ? <LoggedActions /> : <NotLoggedActions />}</Box>;
+  return (
+    <Box>
+      <HStack justifyContent={justifyContentType}>
+        <IconButton
+          bgColor="#fff"
+          color="#4A5568"
+          aria-label="Shopping Cart"
+          icon={<Icon w="6" h="6" as={MdShoppingCart} />}
+          display={{ md: 'block', base: 'none' }}
+        />
+        {user ? <LoggedActions justifyContentType={justifyContentType} /> : <NotLoggedActions justifyContentType={justifyContentType} />}
+      </HStack>
+    </Box>
+  );
 };
