5a9f662f306b05958ea27d99da26ae824aed0c5b
termi95 <56924700+termi95@users.noreply.github.com>
2021-08-28 13:58:14 +0200

    remove react fragment

--- frontend/src/components/layout/SimplyCourseList/simplyCourseTile.tsx
+++ frontend/src/components/layout/SimplyCourseList/simplyCourseTile.tsx
@@ -8,39 +8,37 @@ interface Props {
 }
 
 const SimplyCourseTile: React.FC<Props> = ({ course }) => (
-  <>
-    <Box ml="25%" maxW="250px" key={course.id}>
-      <Link to={`dashboard/course/view/${course.id}`}>
-        <Box
-          minH="320px"
-          mt="20px"
-          mb="20px"
-          boxShadow="md"
-          _hover={{
-            boxShadow: 'xl',
-          }}
-        >
-          <Box height="150px" width="250px">
-            <img src="" alt="Course im placeholder" />
-          </Box>
-          <Center p="15px">
-            <h2>{course.title}</h2>
-          </Center>
-          <Grid templateRows="repeat(3, 1fr)" templateColumns="repeat(1, 1fr)">
-            <GridItem rowSpan={1} colSpan={1} p="10px">
-              Author
-            </GridItem>
-            <GridItem rowSpan={1} colSpan={1} p="10px">
-              Rating
-            </GridItem>
-            <GridItem rowSpan={1} colSpan={1} p="10px">
-              Price
-            </GridItem>
-          </Grid>
+  <Box ml="25%" maxW="250px" key={course.id}>
+    <Link to={`dashboard/course/view/${course.id}`}>
+      <Box
+        minH="320px"
+        mt="20px"
+        mb="20px"
+        boxShadow="md"
+        _hover={{
+          boxShadow: 'xl',
+        }}
+      >
+        <Box height="150px" width="250px">
+          <img src="" alt="Course im placeholder" />
         </Box>
-      </Link>
-    </Box>
-  </>
+        <Center p="15px">
+          <h2>{course.title}</h2>
+        </Center>
+        <Grid templateRows="repeat(3, 1fr)" templateColumns="repeat(1, 1fr)">
+          <GridItem rowSpan={1} colSpan={1} p="10px">
+            Author
+          </GridItem>
+          <GridItem rowSpan={1} colSpan={1} p="10px">
+            Rating
+          </GridItem>
+          <GridItem rowSpan={1} colSpan={1} p="10px">
+            Price
+          </GridItem>
+        </Grid>
+      </Box>
+    </Link>
+  </Box>
 );
 
 export default SimplyCourseTile;
